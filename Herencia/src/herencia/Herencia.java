f96060c48f9040824af3e1c1d1b0980fbb7dbeff
Hugo Villagra <33612966+hv1992@users.noreply.github.com>
2024-04-26 15:55:17 -0400

    Se termina herencia

---
+++ Herencia/src/herencia/Herencia.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package herencia;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hugovillagra
+ */
+public class Herencia {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date fecha = new Date(); //Usa el java.util.Date para crear la fecha
+        
+        System.out.println(fecha.toString());
+    }
+    
+}
