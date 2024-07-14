e85cdb35d53b286d407874c964cbfb3fad8bd287
Hugo Villagra <33612966+hv1992@users.noreply.github.com>
2024-07-13 22:36:52 -0400

    Manejo de JDBC

---
+++ JDBC/ManejoJDBC/src/main/java/datos/Conexion.java
@@ -0,0 +1,39 @@
+package datos;
+
+import java.sql.*;
+
+public class Conexion {
+    //Estos son las variables que se usan para la conexión con la base de datos
+    private static final String JDBC_URL = "jdbc:mysql://localhost/test?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "admin";
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS); //Este es el que realiza la conexión
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+}
